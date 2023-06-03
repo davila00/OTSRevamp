c092f8d2497961b9cddf9b637340c35b02dd3f09
Daniel Avila <deadanny31@gmail.com>
2023-06-02 21:34:17 -0700

    f

---
+++ index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
